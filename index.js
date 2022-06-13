b74255bd0b213791473d73f52627968187d581a8
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-13 09:39:00 +0300

    commit tree 4a0d4bc2d8e30fe61abe6235415e3c6f9efc8852

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+function findMatching(array1, names){
+    return array1.filter((elem) => elem.toLowerCase() === names.toLowerCase() )
+}
+
+function fuzzyMatch(array2, names2){
+    return array2.filter((elem) => elem[0] === names2[0])
+}
+
+function matchName(array3, name3){
+    return array3.filter((elem) => elem.name  === name3)
+}
+
+
+
+
+
+
+
+
+// function findMatching(array1, name){
+//     function truthChecker(name){
+//         return array1.indexOf(name === -1)
+//     }
+//     return array1.filter(truthChecker)
+//     // return array1.filter(()=> array1.includes(name))
+// }
+
+
+// console.log(findMatching(drivers, 'bobby'))
+
+
+
+
+
+
+    // console.log(array1.indexOf(name))
+    
+   
+    // return array1.filter()
